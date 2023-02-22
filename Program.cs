c48a7fdecf2f51dafc7e321201ed819d80195f41
Patrik Ask <ask.patrik@hotmail.com>
2023-02-22 19:58:55 +0100

    Added database support

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ministers_of_sweden.api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Konfigurera program cs klass att aktivera att använda databasen och generera DI möjlighet så vi kan använda det i controllers. 
+
+//Add database support
+
+builder.Services.AddDbContext<MinistersOfSwedenContext>(options => {
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
