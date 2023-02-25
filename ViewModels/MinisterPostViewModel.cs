2357299cb3c9671edf53c25f0f88ca9a2307d935
Patrik Ask <ask.patrik@hotmail.com>
2023-02-25 10:12:10 +0100

    Decorated postviewmodel with requirements

--- ViewModels/MinisterPostViewModel.cs
+++ ViewModels/MinisterPostViewModel.cs
@@ -12,12 +12,19 @@ namespace ministers_of_sweden.api.ViewModels
 
         [Required(ErrorMessage = "Name must be specificed.")]
         public string Name {get; set;}
+         [Required(ErrorMessage = "Type must be specificed.")]
         public string Type{ get; set; }
+        [Required(ErrorMessage = "BirthYear must be specificed.")]
         public int Born {get; set;}
+        [Required(ErrorMessage = "Sex must be specificed.")]
         public string Sex { get; set; }
+         [Required(ErrorMessage = "HasAcademicDegree (true/false) must be specificed.")]
         public bool HasAcademicDegree { get; set; }
+         [Required(ErrorMessage = "Party must be specificed.")]
         public string Party {get; set;}
+         [Required(ErrorMessage = "Department must be specificed.")]
         public string Department {get; set;}
+         [Required(ErrorMessage = "Academic Field must be specificed.")]
         public string AcademicField {get; set;} 
     }
 }
