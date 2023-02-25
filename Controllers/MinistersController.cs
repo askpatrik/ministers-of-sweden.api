714bcc2cf919a5e2527f97f3f96a827236d9cc1d
Patrik Ask <ask.patrik@hotmail.com>
2023-02-25 10:06:36 +0100

    Added HttpPost

--- Controllers/MinistersController.cs
+++ Controllers/MinistersController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ministers_of_sweden.api.Data;
+using ministers_of_sweden.api.Entities;
+using ministers_of_sweden.api.ViewModels;
 
 namespace ministers_of_sweden.api.Controllers
 {
@@ -81,5 +83,58 @@ namespace ministers_of_sweden.api.Controllers
 
             return Ok(result);
         }
+
+        [HttpPost()]
+        public async Task<IActionResult> Add(MinisterPostViewModel minister)
+        {
+            //Kolla om ministern redan har lagts till i systemet. I API returnerar vi då ett korrekt statuskod. 
+            //If-satsen säger att om c.Name är model.Name, lägg till den i C. DÅ är den inte null. 
+            if (await _context.Ministers.SingleOrDefaultAsync(c => c.Name == minister.Name) is not null)         
+                return BadRequest($"Minister med namnet {minister.Name} finns redan i systemet");
+            
+
+            //Hämta department, party och academic field om det stämmer överens med indatan.
+            var politicalDepartment = await _context.Departments.SingleOrDefaultAsync(c => c.Name.ToUpper() == minister.Department.ToUpper());
+            if (politicalDepartment is null) return NotFound($"Could not find department named {minister.Department}");
+                
+
+            var politicalParty = await _context.Parties.SingleOrDefaultAsync(c => c.Name.ToUpper() == minister.Party.ToUpper());
+            if (politicalParty is null) return NotFound($"Could not find party named {minister.Party}");
+
+            var academics = await _context.AcademicFields.SingleOrDefaultAsync(c => c.Name.ToUpper() == minister.AcademicField.ToUpper());
+            if (academics is null) return NotFound($"Could not find academic field named {minister.AcademicField}");
+
+
+            var ministerToAdd = new Minister{
+                Name = minister.Name,
+                Type = minister.Type,
+                Born = minister.Born,
+                Sex = minister.Sex,
+                HasAcademicDegree = minister.HasAcademicDegree,
+                academicField = academics,
+                party = politicalParty,
+                department = politicalDepartment
+            };
+            //Try catch för databasanrops-delen
+
+            try
+            {
+            await _context.Ministers.AddAsync(ministerToAdd);
+            if (await _context.SaveChangesAsync() > 0) return StatusCode(201);
+
+            //Nånting fel med EF core
+            return StatusCode(500, "Internal Server Error");
+            }
+
+            //Ej visa servererrors till klient!
+            catch (Exception ex)
+            {
+                //Loggning till db som hanterar debug info
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal Server Error");
+            }
+         
+        }
     }
+    
 }
